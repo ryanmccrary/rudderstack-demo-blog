542e48a67d336ee0a7b073cf1396cec69ec81bdf
Ryan McCrary <rrmccrary@gmail.com>
2022-02-23 14:34:04 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `A Gatsby Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Eric Dodds`,
+      summary: `who gets your data where it needs to be.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A blog using RudderStack`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ericdodds`,
     },
   },
   plugins: [
@@ -113,8 +113,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `A Gatsby Blog`,
+        short_name: `Eric's Gatsby Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
