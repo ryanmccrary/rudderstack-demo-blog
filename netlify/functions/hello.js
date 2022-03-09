0bdbc66fbdefce7a877e56f66686f42045106e8b
Ryan McCrary <rrmccrary@gmail.com>
2022-03-09 11:32:29 -0500

    example

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -7,12 +7,10 @@ const RUDDER_ENDPOINT = 'https://https://tommyjohnwear-dataplane.rudderstack.com
 const TEST_RUDDER_ENDPOINT = 'https://webhook.site/#!/728e5848-c2d5-4f9a-a97e-cc9b91fdbf15'
 
 exports.handler = async function (event, context) {
-
-  // get response from big commerce api for order
   let response
   try {
     response = await fetch(TEST_API_ENDPOINT)
-
+    // handle response
   } catch (err) {
     return {
       statusCode: err.statusCode || 500,
@@ -22,35 +20,10 @@ exports.handler = async function (event, context) {
     }
   }
 
-  // generate api payload for track event - map from response above
-  // let rudderPayload = {
-  //   "anonymousId": "",
-  //   "event": "purchase",
-  //   "properties": "",
-  //   "context": {
-
-  //   },
-  //   "timestamp": ""
-  // }
-
-  // // send order to rudderstack api
-  // let rudderResponse
-  // try {
-  //   rudderResponse = await fetch(RUDDER_ENDPOINT,{
-  //     method: "POST",
-  //     body: rudderPayload
-  //   })
-  // } catch (err) {
-  //   return {
-  //     statusCode: err.statusCode || 500,
-  //     body: JSON.stringify({
-  //       error: err.message
-  //     })
-  //   }
-  // }
   return {
     statusCode: 200,
-    JSON.stringify({body: response})
+    body: JSON.stringify({
+      data: response
+    })
   }
-
 }
