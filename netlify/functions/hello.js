19c21d565b0f72a857f9e7995a0e0792372c1588
Ryan McCrary <rrmccrary@gmail.com>
2022-03-09 11:20:03 -0500

    json stringify

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -18,7 +18,7 @@ exports.handler = async function (event, context) {
 
     return {
       statusCode: 200,
-      body: response
+      JSON.stringify({body: response})
     }
     // try {
     //   rudderResponse = await fetch(TEST_RUDDER_ENDPOINT,{
